5dd37de2c9e012210869782915a67ce7450ebfd3
Daniel Ran <danielr1735@gmail.com>
2018-08-14 11:04:31 -0400

    add funcionality to button

--- src/index.js
+++ src/index.js
@@ -8,6 +8,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 let listButton = document.getElementById('listButton');
 
-listButton.addEventListener("click", function(){
-  return console.log("yay");
-} )
+listButton.addEventListener("click", displayTaskForm)
+
+function displayTaskForm() {
+  let taskForm = document.createElement('div')
+  taskForm.setAttribute("id", "app-content")
+
+  taskForm.innerHTML = `
+  <form id="create-task-form">
+    <label for="parent-list">Select List:</label>
+    <select id="parent-list">
+    <option value="My Dank List" selected>
+      My Dank List
+    </option>
+    </select>
+
+    <label for="new-task-description">Task description:</label>
+    <input required type="text" id="new-task-description" placeholder="description">
+
+    <label for="new-task-priority">Priority level:</label>
+    <input type="text" id="new-task-priority" placeholder="priority">
+    <input type="submit" value="Create New Task">
+  </form>
+`
+  document.getElementById('app-content').appendChild(taskForm)
+
+  // document.getElementById('app-content').innerHTML =
+
+}
