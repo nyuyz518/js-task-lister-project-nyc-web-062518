df83e46ae7ddf4897dc9c8b31874a7317d6a80b2
Daniel Ran <danielr1735@gmail.com>
2018-08-14 15:10:39 -0400

    added functionality

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const listDiv = document.getElementById("app-content");
   const newListFieldName = document.getElementById('new-list-title')
   const lists = []
-
-  let listButton = document.getElementById('listButton');
+  const listButton = document.getElementById('listButton');
 
   listButton.addEventListener("click", bossfunction)
+
+  function bossfunction(){
+    displayTaskForm();
+    displayLists();
+  }
 
   function generateOptionsTags() {
     return lists.map( listName => `<option value="${listName}">${listName}</option>` ).join('')
   }
 
   function displayTaskForm() {
-    lists.unshift(newListFieldName.value)
-    //let taskForm = document.createElement('div')
 
-    //taskForm.setAttribute("id", "app-content")
+    lists.unshift(newListFieldName.value)
+
     let newTaskForm = `
     <form id="create-task-form">
       <label for="parent-list">Select List:</label>
@@ -37,26 +40,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-  function displayList(){
+  function displayLists(){
     // debugger
-    let newList =
-    `<div id="lists">
-    <div>
-        <h2> ${generateOptionsTags()}
-        <button data-title= class="delete-list">
-          X
-        </button>
-      </h2>
-    </div>
-    </div>`
+    lists.forEach(list => {
+      let newList =
+      `<div id="lists">
+      <div id='${list}'>
+          <h2> ${list}
+          <button id='delete-${list}' class="delete-list">
+            X
+          </button>
+        </h2>
+      </div>
+      </div>`
 
-    listDiv.innerHTML += newList
+      listDiv.innerHTML += newList
+
+      let newListDiv = document.getElementById(`${list}`)
+
+      let deleteBtn = document.getElementById(`delete-${list}`)
+      deleteBtn.addEventListener('click', () => newListDiv.remove())
+
+    })
+
     // newListFieldName.value = ''
 
   }
 
-  function bossfunction(){
-    displayTaskForm();
-    displayList();
-  }
 });
